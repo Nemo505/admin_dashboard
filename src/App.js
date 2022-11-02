c32a7d6b90354dd9df912c686c5ff52282f417ea
unknown <shwechyunaing@xsphere.co>
2022-11-02 16:47:06 +0630

    home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-      Maple
+      <Home/>
     </div>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+        <h1 className="title">Maple Admin</h1>
+    </div>
+  )
+}
+
+export default Home
